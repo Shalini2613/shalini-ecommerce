e968ab1f71b0764d3e3307729e395e8e44d1dcf3
Shalini <akumark12345@gmail.com>
2025-12-21 23:25:36 +0530

    wishlist back buttonadded

--- src/pages/Wishlist.js
+++ src/pages/Wishlist.js
@@ -1,7 +1,12 @@
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 export default function Wishlist() {
   const wishlist = useSelector((state) => state.wishlist);
+  const navigate = useNavigate()
+  const backtocart = () => {
+    navigate("/");
+  }
 
   return (
     <div className="container mt-3">
@@ -15,6 +20,8 @@ export default function Wishlist() {
           <p>₹ {item.price}</p>
         </div>
       ))}
+            <button onClick={()=>backtocart()} className="btn btn-sm btn-primary">Back</button>
+
     </div>
   );
 }
